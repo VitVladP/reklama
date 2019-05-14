f5aa461f32a94f62b11b04806633987300d8b046
vitaly <vit.vlad.p@gmail.com>
2019-05-14 14:18:47 +0300

    1

---
+++ src/main/java/com/reklama/reklama/ReklamaApplication.java
@@ -0,0 +1,13 @@
+package com.reklama.reklama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReklamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReklamaApplication.class, args);
+    }
+
+}
